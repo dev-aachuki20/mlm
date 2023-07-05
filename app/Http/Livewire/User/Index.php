792785c39228637207b225c039f97f978d5183d4
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2023-07-05 09:17:30 +0530

    working on user panel dashboard

--- app/Http/Livewire/User/Index.php
+++ app/Http/Livewire/User/Index.php
@@ -2,11 +2,134 @@
 
 namespace App\Http\Livewire\User;
 
+use DB;
+use Carbon\Carbon;
 use Livewire\Component;
+use App\Models\User;
+use App\Models\Payment;
+use App\Models\Transaction;
 
 class Index extends Component
 {
     protected $layout = null;
+    
+    public $todayEarnings, $last7DaysEarnings, $last30DaysEarnings, $allTimeEarning;
+
+    public $todayEarningPercent = 0, $last7DaysEarningPercent=0, $last30DaysEarningPercent=0, $allTimeEarningPercent = 0;
+
+    public $recentSales = null, $weeklyTopRecords,$monthlyTopRecords,$yearlyTopRecords,$allTimeTopRecords;
+
+    public $levelCommission, $totalWithdrawal, $availableBalance, $netProfit;
+
+    public $incomeGrowthChart, $userId;
+
+    public function mount(){
+        $this->userId = auth()->user()->id;
+        // Start Earning
+
+        //Today
+        $this->todayEarnings = Transaction::whereDate('created_at', today())->where('user_id',$this->userId)->sum('amount');
+
+        //Last 7 days
+        $this->last7DaysEarnings = Transaction::whereDate('created_at', '>=', DB::raw('DATE_SUB(CURDATE(), INTERVAL 7 DAY)'))->where('user_id',$this->userId)->sum('amount');
+
+        $weekStartAmount = Transaction::whereDate('created_at','=',Carbon::now()->subWeek()->startOfWeek())->where('user_id',$this->userId)->sum('amount');
+        $weekEndAmount   = Transaction::whereDate('created_at','=',Carbon::now()->subWeek()->endOfWeek())->where('user_id',$this->userId)->sum('amount');
+
+        $diffreceWeekAmount = (float)$weekEndAmount - (float)$weekStartAmount;
+        if($diffreceWeekAmount != 0){
+            $this->last7DaysEarningPercent = $diffreceWeekAmount / (float)$weekStartAmount * 100;
+        }
+
+        //30 Days
+        $this->last30DaysEarnings = Transaction::whereDate('created_at', '>=', DB::raw('DATE_SUB(CURDATE(), INTERVAL 30 DAY)'))->where('user_id',$this->userId)->sum('amount');
+
+        $monthStartAmount = Transaction::whereDate('created_at','=',Carbon::now()->startOfMonth())->where('user_id',$this->userId)->sum('amount');
+        $monthEndAmount   = Transaction::whereDate('created_at','=',Carbon::now()->endOfMonth())->where('user_id',$this->userId)->sum('amount');
+
+        $diffreceMonthAmount = (float)$monthEndAmount - (float)$monthStartAmount;
+        if($diffreceMonthAmount != 0){
+            $this->last30DaysEarnings = $diffreceMonthAmount / (float)$monthStartAmount * 100;
+        }
+
+        //All Time
+        $this->allTimeEarning = Transaction::sum('amount');
+
+        // New Joiners
+        $this->recentSales = User::with([
+            'roles'=>function($query){
+                $query->where('id',3);
+            }
+        ])->where('referral_user_id',$this->userId)->whereDate('created_at', today())->get();
+        // End Earning
+
+        // Start Commission
+        $this->levelCommission = Transaction::where('payment_type','credit')->where('user_id',$this->userId)->sum('amount');
+        $this->totalWithdrawal = Transaction::where('payment_type','debit')->where('user_id',$this->userId)->sum('amount');
+        $this->availableBalance = (float)$this->levelCommission - (float)$this->totalWithdrawal;
+        $this->netProfit = (float)$this->levelCommission - (float)$this->totalWithdrawal;
+        // End Commission
+
+
+        // Leaderboard 
+
+        DB::statement("SET SQL_MODE=''");
+
+        // Weekly
+        $this->weeklyTopRecords = Transaction::selectRaw('*, SUM(amount) as total_amount')
+        ->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+        ->where('payment_type','credit')
+        ->groupBy('user_id')
+        ->orderByDesc('total_amount')
+        ->limit(5)
+        ->get();
+
+        // Monthly
+        $currentMonth = Carbon::now()->format('Y-m');
+        $this->monthlyTopRecords = Transaction::selectRaw('*, SUM(amount) as total_amount')
+        ->whereRaw("DATE_FORMAT(created_at, '%Y-%m') = ?", [$currentMonth])
+        ->where('payment_type','credit')
+        ->groupBy('user_id')
+        ->orderByDesc('total_amount')
+        ->limit(5)
+        ->get();
+
+        // Yearly
+        $yearStart = date('Y-04-01'); 
+        $yearEnd = date('Y-03-31', strtotime('+1 year'));
+        $this->yearlyTopRecords = Transaction::selectRaw('*, SUM(amount) as total_amount')
+         ->whereBetween('created_at', [$yearStart, $yearEnd])
+         ->where('payment_type','credit')
+         ->groupBy('user_id')
+         ->orderByDesc('total_amount')
+         ->limit(5)
+         ->get();
+
+        // All Time
+        $this->allTimeTopRecords = Transaction::selectRaw('*, SUM(amount) as total_amount')
+        ->where('payment_type','credit')
+        ->groupBy('user_id')
+        ->orderByDesc('total_amount')
+        ->limit(5)
+        ->get();
+
+        //Start income growth chart
+        $firstDayOfMonth = Carbon::now()->firstOfMonth();
+        $lastDayOfMonth  = Carbon::now()->lastOfMonth();
+        $totalWeeks = $lastDayOfMonth->diffInWeeks($firstDayOfMonth) + 1;
+        for($i=1; $i <= $totalWeeks; $i++){
+            $this->incomeGrowthChart['labels'][$i-1] = 'Week '.$i;
+        }
+      
+        // Retrieve records based on current month and week
+        $this->incomeGrowthChart['week_data'] = Transaction::selectRaw('YEAR(created_at) as year, WEEK(created_at) as week_number, SUM(amount) as total_amount')->whereRaw('MONTH(created_at) = ?', [Carbon::now()->month]) 
+            ->whereRaw('WEEK(created_at) = ?', [Carbon::now()->weekOfYear])
+            ->where('payment_type','credit')
+            ->groupBy('week_number')->pluck('total_amount')->toArray();
+        //End income growth chart
+
+    }
+
     public function render()
     {
         return view('livewire.user.index');
--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -3,13 +3,16 @@
     <div class="col-md-12 grid-margin">
       <div class="watch-our-introductory">
         <div class="introductory-text">
-          <h1>Watch Our Introductory Video!</h1>
-          <p>The vision of MyFutureBiz is to develop entrepreneurial mindset and create financially independent person's excellent.</p>
+          <h1>{{ getSetting('introduction_video_title') }}</h1>
+          <p>{!! getSetting('introduction_video_description') !!}</p>
         </div>
         <div class="introductory-video">
+          @php
+            $introVideo = getSettingDetail('introduction_video');
+            $introVideoImage = getSetting('introduction_video_image');
+          @endphp
           <div class="box-video">
-            {{-- <div class="bg-video" style="background-image: url(images/package.png);"> --}}
-            <div class="bg-video">
+          <div class="bg-video" style="background-image: url({{ $introVideoImage ? $introVideoImage : asset('images/package.png') }});">
               <div class="bt-play">
                 <svg width="55" height="55" viewBox="0 0 55 55" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M41.7108 27.5123L20.4197 13.9199V41.1046L41.7108 27.5123Z" fill="white"></path>
@@ -21,13 +24,23 @@
               </div>
             </div>
             <div class="video-container">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/DGFvSDGUPCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
+
+              @if($introVideo->video)
+                <video controls="" width="420" height="315" preload="none" poster="{{ $introVideoImage ? $introVideoImage : asset('images/package.png') }}" id="introductionVideo"  playsinline>
+                  <source class="js-video" src="{{ $introVideo->video_url }}" type="video/{{ $introVideo->video->extension }}">
+                </video>
+              @else
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/DGFvSDGUPCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen=""></iframe>
+              @endif
+
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  {{-- Start package selling overview --}}
   <div class="row row-gap-30">
     <div class="col-lg-3 col-md-4 col-sm-12">
       <div class="old-lace card-box">
@@ -37,12 +50,22 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/1.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 250.00</div>
+        <div class="price-box">&#8377 {{ number_format($todayEarnings,2) }}</div>
         <div class="box-discrip">
-          <span class="text-green">
-            <img src="{{ asset('images/icons/up-graph.svg') }}">
-            18%
-          </span>Today's Earning</div>
+          
+           @if($todayEarningPercent > 0)
+              <span class="text-green">
+                <img src="{{ asset('images/icons/up-graph.svg') }}">
+                {{$todayEarningPercent}}%
+              </span>
+            @else
+            <span class="text-red">
+              <img src="{{ asset('images/icons/down-graph.svg') }}">
+              {{$todayEarningPercent}}%
+            </span>
+            @endif
+
+            Today's Earning</div>
       </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-12">
@@ -53,12 +76,20 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/2.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 6500.00</div>
+        <div class="price-box">&#8377 {{ number_format($last7DaysEarnings,2) }}</div>
         <div class="box-discrip">
-          <span class="text-green">
-            <img src="{{ asset('images/icons/up-graph.svg') }}">
-            18%
-          </span>Last 7 Days Earning</div>
+           @if($last7DaysEarningPercent > 0)
+              <span class="text-green">
+                <img src="{{ asset('images/icons/up-graph.svg') }}">
+                {{$last7DaysEarningPercent}}%
+              </span>
+            @else
+            <span class="text-red">
+              <img src="{{ asset('images/icons/down-graph.svg') }}">
+              {{$last7DaysEarningPercent}}%
+            </span>
+            @endif
+          Last 7 Days Earning</div>
       </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-12">
@@ -69,12 +100,20 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/3.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 6500.00</div>
+        <div class="price-box">&#8377 {{ number_format($last30DaysEarnings,2) }}</div>
         <div class="box-discrip">
-          <span class="text-red">
-            <img src="{{ asset('images/icons/down-graph.svg') }}">
-            06%
-          </span>30 Days Earning</div>
+           @if($last30DaysEarningPercent > 0)
+              <span class="text-green">
+                <img src="{{ asset('images/icons/up-graph.svg') }}">
+                {{$last30DaysEarningPercent}}%
+              </span>
+            @else
+            <span class="text-red">
+              <img src="{{ asset('images/icons/down-graph.svg') }}">
+              {{$last30DaysEarningPercent}}%
+            </span>
+            @endif
+          30 Days Earning</div>
       </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-12">
@@ -85,12 +124,20 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/4.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 166500.00</div>
+        <div class="price-box">&#8377 {{ number_format($allTimeEarning,2) }}</div>
         <div class="box-discrip">
-          <span class="text-green">
-            <img src="{{ asset('images/icons/up-graph.svg') }}">
-            18%
-          </span>All Time Earning</div>
+          @if($allTimeEarningPercent > 0)
+            <span class="text-green">
+              <img src="{{ asset('images/icons/up-graph.svg') }}">
+              {{$allTimeEarningPercent}}%
+            </span>
+          @else
+          <span class="text-red">
+            <img src="{{ asset('images/icons/down-graph.svg') }}">
+            {{$allTimeEarningPercent}}%
+          </span>
+          @endif
+          All Time Earning</div>
       </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-12">
@@ -101,8 +148,8 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/5.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 2000.00</div>
-        <div class="box-discrip">Passive Income</div>
+        <div class="price-box">&#8377 {{ number_format($levelCommission,2) }}</div>
+        <div class="box-discrip">Level Commission</div>
       </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-12">
@@ -113,7 +160,7 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/6.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 2500.00</div>
+        <div class="price-box">&#8377 {{ number_format($totalWithdrawal,2) }}</div>
         <div class="box-discrip">Total Withdrawal</div>
       </div>
     </div>
@@ -123,7 +170,7 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/7.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 615500.00</div>
+        <div class="price-box">&#8377 {{ number_format($availableBalance,2) }}</div>
         <div class="box-discrip">Available Balance</div>
       </div>
     </div>
@@ -135,11 +182,13 @@
         <div class="bg-white icon-main">
           <img src="{{ asset('images/icons/8.svg') }}" alt="1">
         </div>
-        <div class="price-box">&#8377 156500.00</div>
-        <div class="box-discrip">Net Balance</div>
+        <div class="price-box">&#8377 {{ number_format($netProfit,2)}}</div>
+        <div class="box-discrip">Net Profit</div>
       </div>
     </div>
   </div>
+  {{-- End package selling overview --}}
+
   <hr>
 
   <div class="row">
@@ -164,76 +213,22 @@
          </div>
          <div class="recent-list">
            <ul class="icon-data-list">
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
-            <li>
-              <div class="d-flex recent-list-detail">
-                <img src="{{ asset('images/face21.jpg') }}" alt="user">
-                <div class="profile-name-re">Cameron Williamson</div>
-              </div>                      
-              <div class="price-recent">&#8377 1500</div>
-            </li>
+           @if($recentSales->count() > 0)
+             @foreach($recentSales as $sale)
+              <li>
+                <div class="d-flex recent-list-detail">
+                  <img src="{{ $newJoiner->profile_image_url ? $sale->profile_image_url : asset(config('constants.default.profile_image')) }}" alt="user">
+                  <div class="profile-name-re">{{ ucwords($sale->name) }}</div>
+                </div>
+                @php
+                  $amount = $sale->payments()->sum('amount');
+                @endphp                      
+                <div class="price-recent">&#8377 {{ number_format($amount,2) }}</div>
+              </li>
+             @endforeach
+             @else
+             <li>No Record Found!</li>
+            @endif
           </ul>
          </div>
         </div>
@@ -250,7 +245,7 @@
           </div>
           <div class="row row-gap-30">
             <div class="col-lg-3 col-md-4 col-sm-12">
-              <p class="small-title">This Week (19 - 25 Jun)</p>
+              <p class="small-title">This Week ({{\Carbon\Carbon::now()->startOfWeek()->format('d')}} - {{\Carbon\Carbon::now()->endOfWeek()->format('d')}} {{\Carbon\Carbon::now()->format('M')}})</p>
               <div class="recent-list small-text h-auto">
                 <ul class="icon-data-list">
                   <li>
@@ -292,7 +287,7 @@
               </div>                  
             </div>
             <div class="col-lg-3 col-md-4 col-sm-12">
-              <p class="small-title">This Month (1 - 30 Jun)</p>
+              <p class="small-title">This Month ({{\Carbon\Carbon::now()->startOfMonth()->format('d')}} - {{\Carbon\Carbon::now()->endOfMonth()->format('d')}} {{\Carbon\Carbon::now()->format('M')}})</p>
               <div class="recent-list small-text h-auto">
                 <ul class="icon-data-list">
                   <li>
@@ -334,7 +329,7 @@
               </div>                 
             </div>
             <div class="col-lg-3 col-md-4 col-sm-12">
-              <p class="small-title">Yearly (MAR - FEB)</p>
+              <p class="small-title">Yearly ({{getFinancialYearMonths()[0]}} - {{getFinancialYearMonths()[11]}})</p>
               <div class="recent-list small-text h-auto">
                 <ul class="icon-data-list">
                   <li>
